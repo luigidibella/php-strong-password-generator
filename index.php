4f3be3a8a8600c3510c68491823748932efafc2d
luigidibella <lykos.omega@gmail.com>
2024-04-16 16:34:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css' integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==' crossorigin='anonymous'/>
+  <title>PHP Strong Password Generator</title>
+</head>
+<body>
+  <div class="container text-center text-white py-5 bg-primary">
+    <h1>Strong Password Generator</h1>
+    <h2>Genera una password sicura</h2>
+
+  </div>
+  
+</body>
+</html>
