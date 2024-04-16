f58e67bf63d6e3ed17ccb88199fac2c097285a18
luigidibella <lykos.omega@gmail.com>
2024-04-16 17:22:58 +0200

    [add] milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+
+  function generaPasswordCasuale($lunghezza) {
+      $caratteri = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}|[]\:";<>,.?/';
+      $lunghezzaCaratteri = strlen($caratteri);
+      $password = '';
+
+      for ($i = 0; $i < $lunghezza; $i++) {
+          $index = rand(0, $lunghezzaCaratteri - 1);
+          $password .= $caratteri[$index];
+      }
+
+      return $password;
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +27,30 @@
   <div class="container text-center text-white py-5 bg-primary">
     <h1>Strong Password Generator</h1>
     <h2>Genera una password sicura</h2>
+
+    <div class="alert alert-primary" role="alert">
+      <?php
+
+        if (isset($_GET['lunghezza'])) {
+            $lunghezza = intval($_GET['lunghezza']);
+            if ($lunghezza > 0) {
+                $passwordGenerata = generaPasswordCasuale($lunghezza);
+                echo "<h2>Password Generata:</h2>";
+                echo "<p>$passwordGenerata</p>";
+            } else {
+                echo "<p style='color: red;'>Nessun parametro valido inserito.</p>";
+            }
+        }
+
+      ?>
+    </div>
+
+
+    <form action="index.php" method="get">
+        <label for="lunghezza">Lunghezza della Password:</label>
+        <input type="number" id="lunghezza" name="lunghezza" min="1" required>
+        <button type="submit">Genera Password</button>
+    </form>
 
   </div>
   
